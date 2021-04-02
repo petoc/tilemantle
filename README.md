c5c7fd833485f2262e10647936e4f2400982cfa6
Peter C <petoc@users.noreply.github.com>
2021-04-02 19:54:47 +0200

    Bumped dependencies, fixed issues and added expired list option, updated README.md

--- README.md
+++ README.md
@@ -13,18 +13,21 @@ $ tilemantle -h
 Usage: tilemantle <url> [<url> ...] [options]
 
 Options:
-  --version          Display version number
-  -h, --help         Display usage information
-  -l, --list         Don't perform any requests, just list all tile URLs
-  -z, --zoom         Zoom levels (comma separated or range)
-  -e, --extent       Extent of region in the form of: nw_lat,nw_lon,se_lat,se_lon
-  -p, --point        Center of region (use in conjunction with -b)
-  -b, --buffer       Buffer point/geometry by an amount. Affix units at end: mi,km
-  -d, --delay        Delay between requests. Affix units at end: ms,s
-  -m, --method       HTTP method to use to fetch tiles
-  -r, --retries      Number of retries
-  -H, --header       Set request header                                        
-  -c, --concurrency  Number of tiles to request simultaneously
+  --version            Display version number
+  -h, --help           Display usage information
+  -l, --list           Don't perform any requests, just list all tile URLs
+  -a, --allowfailures  Skip failures, keep on truckin'
+  -z, --zoom           Zoom levels (comma separated list, range zmin-zmax or reversed range zmax-zmin)
+  -e, --extent         Extent of region in the form of: nw_lat,nw_lon,se_lat,se_lon
+  -f, --file           GeoJSON file on disk to use as geometry
+  -p, --point          Center of region (use in conjunction with -b)
+  -b, --buffer         Buffer point/geometry by an amount. Affix units at end: mi,km
+  -d, --delay          Delay between requests. Affix units at end: ms,s
+  -r, --retries        Number of retries
+  -m, --method         HTTP method to use to fetch tiles
+  -H, --header         Set request header
+  -c, --concurrency    Number of tiles to request simultaneously
+  -x, --expiredlist    Expired tiles list from osm2pgsql command
 ```
 
 ### Usage
@@ -37,7 +40,13 @@ $ tilemantle http://myhost.com/{z}/{x}/{y}.png --point=44.523333,-109.057222 --b
 $ tilemantle http://myhost.com/{z}/{x}/{y}.png --extent=44.523333,-109.057222,41.145556,-104.801944 -z 10-14
 
 # use a geojson geometry as bounds
-$ cat region.geojson | tilemantle http://myhost.com/{z}/{x}/{y}.png -z 10-14
+$ tilemantle http://myhost.com/{z}/{x}/{y}.png --zoom=10-14 -f region.geojson
+
+# use a geojson geometry as bounds with reversed zoom level warming
+$ tilemantle http://myhost.com/{z}/{x}/{y}.png --zoom=14-10 -f region.geojson
+
+# use an expired tiles list from osm2pgsql command
+$ tilemantle http://myhost.com/{z}/{x}/{y}.png --expiredlist expired.list
 
 # use a geojson geometry as bounds + buffer by distance
 $ cat region.geojson | tilemantle http://myhost.com/{z}/{x}/{y}.png --buffer=20mi -z 10-14
--- bin/tilemantle.js
+++ bin/tilemantle.js
@@ -1,51 +1,58 @@
 #!/usr/bin/env node
+
 var fs = require('fs');
+var es = require('event-stream');
 var yargs = require('yargs');
 var async = require('async');
 var chalk = require('chalk');
 var path = require('path');
-var turf = require('turf');
+var turf = require('@turf/turf');
 var numeral = require('numeral');
 var request = require('request');
-var tilecover = require('tile-cover');
+var tilecover = require('@mapbox/tile-cover');
 var humanizeDuration = require('humanize-duration');
 var pkg = require('../package.json');
 var ProgressBar = require('progress');
 var bar;
 
-var filesize = function(bytes) {
+var filesize = function (bytes) {
 	return Number((bytes / 1024).toFixed(2)) + 'kB';
 };
 
-var argv = require('yargs')
+var argv = yargs(process.argv.slice(2))
 	.usage('Usage: $0 <url> [<url> ...] [options]')
-	.version(pkg.version+'\n', 'version', 'Display version number')
+	.version('version', 'Display version number', pkg.version)
 	.alias('h', 'help').describe('h', 'Display usage information').boolean('h')
 	.alias('l', 'list').describe('l', 'Don\'t perform any requests, just list all tile URLs').boolean('l')
-	.alias('a', 'allowfailures').describe('a', 'Skip failures, keep on truckin\'').boolean('a')
-	.alias('z', 'zoom').describe('z', 'Zoom levels (comma separated or range)').string('z')
+	.alias('a', 'allowfailures').describe('a', 'Skip failures, keep on truckin\'').boolean('a').default('allowfailures', true)
+	.alias('z', 'zoom').describe('z', 'Zoom levels (comma separated list, range zmin-zmax or reversed range zmax-zmin)').string('z')
 	.alias('e', 'extent').describe('e', 'Extent of region in the form of: nw_lat,nw_lon,se_lat,se_lon').string('e')
 	.alias('f', 'file').describe('f', 'GeoJSON file on disk to use as geometry').string('f')
 	.alias('p', 'point').describe('p', 'Center of region (use in conjunction with -b)').string('p')
 	.alias('b', 'buffer').describe('b', 'Buffer point/geometry by an amount. Affix units at end: mi,km').string('b')
-	.alias('d', 'delay').describe('d', 'Delay between requests. Affix units at end: ms,s').string('d')
-	.alias('r', 'retries').describe('r', 'Number of retries')
-	.alias('m', 'method').describe('m', 'HTTP method to use to fetch tiles').string('m')
+	.alias('d', 'delay').describe('d', 'Delay between requests. Affix units at end: ms,s').string('d').default('delay', '100ms')
+	.alias('r', 'retries').describe('r', 'Number of retries').default('retries', 1)
+	.alias('m', 'method').describe('m', 'HTTP method to use to fetch tiles').string('m').default('method', 'HEAD')
 	.alias('H', 'header').describe('H', 'Add a request header').string('H')
-	.alias('c', 'concurrency').describe('c', 'Number of tiles to request simultaneously')
-	.default({delay: '100ms', concurrency: 1, allowfailures: true, retries: 5, method: 'HEAD'})
-	.check(function(argv) {
+	.alias('c', 'concurrency').describe('c', 'Number of tiles to request simultaneously').default('concurrency', 1)
+	.alias('x', 'expiredlist').describe('x', 'Expired tiles list from osm2pgsql command').string('x')
+	.check(function (argv) {
 		if (!/^\d+(\.\d+)?(ms|s)$/.test(argv.delay)) throw new Error('Invalid "delay" argument');
-		if (!/^((\d+\-\d+)|(\d+(,\d+)*))$/.test(argv.zoom)) throw new Error('Invalid "zoom" argument');
+		if (typeof argv.zoom !== 'undefined' && !/^((\d+\-\d+)|(\d+(,\d+)*))$/.test(argv.zoom)) throw new Error('Invalid "zoom" argument');
+		return true;
 	})
-	.parse(process.argv);
+	.argv;
 
 function displayHelp() {
 	yargs.showHelp();
+	console.log('');
 	console.log('Examples:');
+	console.log('');
 	console.log('  $ tilemantle http://myhost.com/{z}/{x}/{y}.png --point=44.523333,-109.057222 --buffer=12mi --zoom=10-14');
 	console.log('  $ tilemantle http://myhost.com/{z}/{x}/{y}.png --extent=44.523333,-109.057222,41.145556,-104.801944 --zoom=10-14');
 	console.log('  $ tilemantle http://myhost.com/{z}/{x}/{y}.png --zoom=10-14 -f region.geojson');
+	console.log('  $ tilemantle http://myhost.com/{z}/{x}/{y}.png --zoom=14-10 -f region.geojson');
+	console.log('  $ tilemantle http://myhost.com/{z}/{x}/{y}.png --expiredlist expired.list');
 	console.log('  $ cat region.geojson | tilemantle http://myhost.com/{z}/{x}/{y}.png --zoom=10-14');
 	console.log('  $ cat region.geojson | tilemantle http://myhost.com/{z}/{x}/{y}.png --buffer=20mi --zoom=10-14');
 	console.log('');
@@ -57,8 +64,8 @@ if (argv.help) {
 }
 
 // parse options
-var urltemplates = argv._.slice(2);
-var validateurlparam = function(template, param) {
+var urltemplates = argv._;
+var validateurlparam = function (template, param) {
 	if (template.indexOf(param) === -1) {
 		displayHelp();
 		console.error('URL missing ' + param + ' parameter');
@@ -66,7 +73,7 @@ var validateurlparam = function(template, param) {
 	}
 };
 
-urltemplates.forEach(function(template) {
+urltemplates.forEach(function (template) {
 	if (!/^https?\:/.test(template)) {
 		displayHelp();
 		console.error('No url template provided');
@@ -85,17 +92,17 @@ var t_start = (new Date()).getTime();
 
 async.series([
 	function readFromPipe(callback) {
-		process.stdin.on('readable', function() {
-			var chunk = this.read();
-			if (chunk === null) {
-				callback();
-			} else {
-				rawgeojson += chunk;
+		try {
+			rawgeojson = fs.readFileSync(process.stdin.fd, 'utf8');
+			if (rawgeojson.trim() == "") {
+				throw new Error('invalid input');
 			}
-		});
-		process.stdin.on('end', function() {
-			callback();
-		});
+		} catch (err) {
+			if (err.code !== 'EAGAIN') {
+				throw err;
+			}
+		}
+		callback();
 	},
 	function determineGeometry(callback) {
 		if (rawgeojson) {
@@ -107,104 +114,62 @@ async.series([
 			geojson = turf.point([coords[1], coords[0]]);
 		} else if (argv.extent) {
 			var coords = String(argv.extent).split(',').map(parseFloat);
-			var input = turf.featurecollection([
+			var input = turf.featureCollection([
 				turf.point([coords[1], coords[0]]),
 				turf.point([coords[3], coords[2]])
 			]);
-			geojson = turf.extent(input);
+			geojson = turf.bboxPolygon(turf.bbox(input));
+		} else if (argv.expiredlist) {
+			callback();
+			return true;
 		} else {
 			displayHelp();
-			console.error('No geometry provided. Pipe geojson, or use --point or --extent');
+			console.error('No geometry provided. Pipe geojson, or use --point / --extent / --expiredlist');
 			return process.exit(1);
 		}
-
 		if (argv.buffer) {
 			var radius = parseFloat(argv.buffer);
 			var units = /mi$/.test(argv.buffer) ? 'miles' : 'kilometers';
 			geojson = turf.buffer(geojson, radius, units);
 		}
-
+		if (!geojson.type) {
+			throw new Error('Missing geometry type');
+		}
 		// tilecover doesn't like features
-		geojson = turf.merge(geojson);
+		if (geojson.type === 'FeatureCollection') {
+			var merged = geojson.features[0];
+			for (var i = 1; i < geojson.features.length; i++) {
+				if (geojson.features[i].geometry) {
+					merged = turf.union(merged, geojson.features[i]);
+				}
+			}
+			geojson = merged;
+		}
 		if (geojson.type === 'Feature') {
 			geojson = geojson.geometry;
 		}
-
 		callback();
 	},
 	function performAction(callback) {
-		var zooms = [];
-		if (argv.zoom.indexOf('-') > -1) {
-			var parts = argv.zoom.split('-').map(Number);
-			var minzoom = parts[0];
-			var maxzoom = parts[1];
-			for (var z = minzoom; z <= maxzoom; z++) {
-				zooms.push(z);
-			}
-		} else {
-			zooms = argv.zoom.split(',').map(Number);
-			zooms.sort();
-		}
-
-		function buildTileList(geojson, zooms) {
-			var groups = [];
-			zooms.forEach(function(z) {
-				groups.push(tilecover.tiles(geojson, {min_zoom: z, max_zoom: z}));
-			});
-			var result = [];
-			return result.concat.apply(result, groups);
-		}
-		function buildURLs(xyz) {
-			urltemplates.forEach(function(template) {
-				urls.push(template.replace(/\{x\}/g, xyz[0]).replace(/\{y\}/g, xyz[1]).replace(/\{z\}/g, xyz[2]));
-			});
-		}
-
+		var headers = {};
 		var urls = [];
-		buildTileList(geojson, zooms).forEach(buildURLs);
-
-		if (argv.list) {
-			for (var i = 0, n = urls.length; i < n; i++) {
-				console.log(urls[i]);
-			}
-			callback();
-		} else {
-			// build request headers
-			var headers = {};
-			if (argv.header) {
-				if (!Array.isArray(argv.header)) argv.header = [argv.header];
-				argv.header.forEach(function(header) {
-					var delim = header.indexOf(':');
-					if (delim === -1) return;
-					var key = header.substring(0, delim).trim();
-					var value = header.substring(delim + 1).trim();
-					headers[key] = value;
-				});
-			}
-			if (!headers['User-Agent']) {
-				headers['User-Agent'] = 'TileMantle/' + pkg.version;
-			}
-
-			bar = new ProgressBar(chalk.gray('[:bar] :percent (:current/:total) eta: :etas'), {total: urls.length, width: 20});
-			async.eachLimit(urls, argv.concurrency, function(url, callback) {
-				async.retry(argv.retries, function(callback) {
+		function asyncRequest(urls, limit, callback) {
+			async.eachLimit(urls, limit, function (url, callback) {
+				async.retry(argv.retries, function (callback) {
 					var start = (new Date()).getTime();
 					request({
 						method: argv.method,
 						url: url,
 						headers: headers
-					}, function(err, res, body) {
+					}, function (err, res, body) {
 						if (err) return callback(err);
-
 						var time = (new Date()).getTime() - start;
 						var statuscolor = res.statusCode !== 200 ? 'red' : 'green';
 						var size_data = filesize(res.body.length);
 						var size_length = res.headers['content-length'] ? filesize(Number(res.headers['content-length'])) : '(no content-length)';
-
 						process.stdout.cursorTo(0);
 						console.log(chalk.gray('[') + chalk[statuscolor](res.statusCode) + chalk.grey(']') + ' ' + url + ' ' + chalk.blue(time + 'ms') + ' ' + chalk.grey(size_data + ', ' + size_length));
 						bar.tick();
-
 						if (res.statusCode !== 200) {
 							count_failed++;
 							callback('Request failed (non-200 status)');
@@ -213,14 +178,136 @@ async.series([
 							callback();
 						}
 					});
-				}, function(err) {
+				}, function (err) {
 					if (err && argv.allowfailures) err = null;
 					callback(err);
 				});
 			}, callback);
 		}
+
+		function buildURLs(xyz, urls) {
+			urltemplates.forEach(function (template) {
+				urls.push(template.replace(/\{x\}/g, xyz[0]).replace(/\{y\}/g, xyz[1]).replace(/\{z\}/g, xyz[2]));
+			});
+		}
+		if (argv.header) {
+			if (!Array.isArray(argv.header)) argv.header = [argv.header];
+			argv.header.forEach(function (header) {
+				var delim = header.indexOf(':');
+				if (delim === -1) return;
+				var key = header.substring(0, delim).trim();
+				var value = header.substring(delim + 1).trim();
+				headers[key] = value;
+			});
+		}
+		if (!headers['User-Agent']) {
+			headers['User-Agent'] = 'TileMantle/' + pkg.version;
+		}
+		if (argv.expiredlist) {
+			var lineCount = 0;
+			fs.createReadStream(argv.expiredlist).on('data', function (chunk) {
+				for (i = 0; i < chunk.length; ++i) {
+					if (chunk[i] === 10) {
+						lineCount++;
+					}
+				}
+			}).on('end', function () {
+				lineCount = lineCount * urltemplates.length;
+				bar = new ProgressBar(chalk.gray('[:bar] :percent (:current/:total) eta: :etas'), {
+					total: lineCount,
+					width: 20
+				});
+				var lineNumber = 0;
+				var rs = fs.createReadStream(argv.expiredlist)
+					.pipe(es.split())
+					.pipe(es.mapSync(function (line) {
+							rs.pause();
+							lineNumber++;
+							if (line.trim().length === 0) {
+								return rs.resume();
+							}
+							var zxy = line.split('/');
+							buildURLs([zxy[1], zxy[2], zxy[0]], urls);
+							if (argv.list) {
+								for (var i = 0, n = urls.length; i < n; i++) {
+									console.log(urls[i]);
+								}
+								urls = [];
+								return rs.resume();
+							}
+							if (urls.length > 0 && lineNumber % argv.concurrency === 0) {
+								asyncRequest(urls, argv.concurrency, function (err) {
+									urls = [];
+									rs.resume();
+								});
+							} else {
+								rs.resume();
+							}
+						})
+						.on('error', function (err) {
+							callback(err);
+						})
+						.on('end', function () {
+							if (urls.length > 0) {
+								asyncRequest(urls, argv.concurrency, function (err) {
+									urls = [];
+									callback();
+								});
+							} else {
+								callback();
+							}
+						})
+					);
+			});
+		} else {
+			var zooms = [];
+			if (argv.zoom.indexOf('-') > -1) {
+				var parts = argv.zoom.split('-').map(Number);
+				var minzoom = parts[0];
+				var maxzoom = parts[1];
+				if (maxzoom > minzoom) {
+					for (var z = minzoom; z <= maxzoom; z++) {
+						zooms.push(z);
+					}
+				} else {
+					for (var z = minzoom; z >= maxzoom; z--) {
+						zooms.push(z);
+					}
+				}
+			} else {
+				zooms = argv.zoom.split(',').map(Number);
+				zooms.sort();
+			}
+
+			function buildTileList(geojson, zooms) {
+				var groups = [];
+				zooms.forEach(function (z) {
+					groups.push(tilecover.tiles(geojson, {
+						min_zoom: z,
+						max_zoom: z
+					}));
+				});
+				var result = [];
+				return result.concat.apply(result, groups);
+			}
+			buildTileList(geojson, zooms).forEach(function (xyz) {
+				return buildURLs(xyz, urls);
+			});
+			if (argv.list) {
+				for (var i = 0, n = urls.length; i < n; i++) {
+					console.log(urls[i]);
+				}
+				callback();
+			} else {
+				bar = new ProgressBar(chalk.gray('[:bar] :percent (:current/:total) eta: :etas'), {
+					total: urls.length,
+					width: 20
+				});
+				asyncRequest(urls, argv.concurrency, callback);
+			}
+		}
 	}
-], function(err) {
+], function (err) {
 	if (count_succeeded || count_failed) {
 		var duration = (new Date()).getTime() - t_start;
 		console.log('');
